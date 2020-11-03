dd24f44c5c33e81e25aab12b46c60651a46956f6
Wilson McDade <wilson.mcdade@gmail.com>
2020-11-03 08:08:36 -0500

    Make the file more readable

--- gokart.ino
+++ gokart.ino
@@ -3,8 +3,6 @@
 //definitions
 #define PWM_CYCLE 50.0
 #define PWM_OUT 9
-#define POT_READ A0
-#define ON_OFF 13
 #define MAX_DUTY 869
 #define MIN_DUTY 0
 #define inPin 12
@@ -14,10 +12,6 @@ int speedLevel;
 int ACC = 0;
 int reading;
 int previous = LOW;
-
-//need these to be longs because milliseconds
-long time = 0;
-long debounce = 200;
 
 void setup()
 {
@@ -33,31 +27,20 @@ void setup()
 
 void loop() {
   reading = digitalRead(inPin);
+  
   //if switch is in on position, accelerate
   if (reading == HIGH && previous == LOW) {
-  ACC = ACC + 20;
-  //delay(500);
+    ACC = ACC + 20;
   }
+  
   //if switch is in off position, decelerate
   if (reading == LOW && previous == HIGH) {
-  ACC = ACC - 20;
-  //delay(500);
+    ACC = ACC - 20;
   }
-  //if (ACC >= 1000) {
-  //ACC = 0;
-  //}
-  //if speed coef is higher than max duty then set it equal to max duty
-  //if (ACC > MAX_DUTY) {
-  //ACC = MAX_DUTY;
-  //}
-  //If I want to use a poteniometer I can use these scripts to map pot readings to pwm duty
-  //potGo = analogRead(POT_READ);
+  
   speedLevel = map(ACC, 0, 1023, 0, MAX_DUTY);
 
-  time = millis();
   Timer1.setPwmDuty(PWM_OUT, speedLevel);
   Serial.print("speed: " + speedLevel);
   Serial.print("ACC: " + ACC);
-  //Serial.print("reading: " + reading);
-  //previous = reading;
 }
